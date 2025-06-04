4e22347255446ea0d41a650da9d0ac9e8f47c09b
Dev Nadson <nadson.pros@gmail.com>
2025-06-03 21:04:17 -0300

    Arquivos renomeados e realocados

---
+++ build/server/menu.py
@@ -0,0 +1,46 @@
+import os
+from server.crud import * #Não funciona nesse arquivo, mas funciona na main
+
+def Option_INT(prompt: str) -> int:
+    while True:
+        try:
+            return int(input(prompt))
+        except ValueError:
+            print("Por favor, insira um número válido.")
+
+def create_menu():
+    while True:
+        print("\n----- Criação de Pacientes -----")
+        print("1. Criar paciente")
+        print("2. Sair\n")
+
+        option = Option_INT("Selecione uma opção: ")
+        match option:
+            case 1:
+                create_patient()
+            case _:
+                break
+
+def main():
+    main_menu = {
+        1: create_menu,
+        2: read_patients,
+        3: update_patient,
+        4: remove_patient
+    }
+    while True:
+        print("----- Gerenciamento de Pacientes -----\n")
+        print("1. Criar novo paciente")
+        print("2. Ler o prontuário")
+        print("3. Atualizar o prontuário")
+        print("4. Excluir algum paciente")
+        print("5. Sair\n")
+
+        option = Option_INT("Selecione uma opção: ")
+        if option == 5:
+            break
+        main_menu.get(option, invalid_option)()
+
+os.system('cls')
+if __name__ == "__main__":
+    main()
